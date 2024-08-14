150c164faeaab3446625c5cc13be2c5b0c6f1c79
theTrueEnder <67336179+theTrueEnder@users.noreply.github.com>
2024-08-13 20:39:49 -0400

    Python file support
    Python code decommenting and recommenting developed and tested
    CLI created and moved to cli.py

---
+++ cli.py
@@ -0,0 +1,38 @@
+from decomment import *
+import argparse
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument(
+    "--mode", 
+    "-m",
+    action="store", 
+    choices=["decomment", "recomment"]
+)
+
+parser.add_argument(
+    "--file", 
+    "-f",
+    action="store", 
+    help="Filename or path to the target file",
+    nargs="?"
+)
+
+args = parser.parse_args()
+
+fname = args.file
+# args.mode = 'decomment'
+# fname = 'code.py'
+
+if __name__ == '__main__':
+    ext = fname[fname.find('.')+1:]
+    match(ext):
+        case 'py':
+            if args.mode == 'decomment':
+                print(f'Decommenting {fname}...')
+                dc_python(fname)
+            elif args.mode == 'recomment':
+                print(f'Recommenting {fname}...')
+                rc_python(fname)
+        case _:
+            print(f'Filetype "{ext}" not supported.')
