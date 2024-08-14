eaad846d9b667b0f29283877b33a29758cd1b9dc
theTrueEnder <67336179+theTrueEnder@users.noreply.github.com>
2024-08-13 23:47:26 -0400

    Added style lang options, heavy restructure of decomment for block comments
    Added dicts for c-style comment langs and python-style comment langs (will uncomment as I can confirm that the program works). Block comments are difficult to cover all situations for. Code untested currently.

--- cli.py
+++ cli.py
@@ -22,16 +22,66 @@ parser.add_argument(
 args = parser.parse_args()
 fname = args.file
 
+cstyle_langs = {
+    ".c",  # C 
+    ".cpp", ".cc", ".cxx", ".h", ".hpp", ".hh",  # C++
+    ".cs",  # C#
+    ".java",  # java
+    ".js", ".mjs",  # javascript
+    ".m", ".mm", ".h",  # objective-c
+    ".go",  # go
+    ".php", ".phtml",  # php
+    ".rs",  # rust
+    ".swift",  # swift
+    ".scala", ".sc", ".sbt",  # scala
+    ".kt", ".kts",  # kotlin
+    ".d",  # d
+    ".ts", ".tsx",  # typescript
+    ".as",  # actionscript
+    ".hx",  # haxe
+    ".groovy", ".gvy", ".gy", ".gsh"  # groovy
+    
+}
+
+pythonstyle_langs = {
+    ".py",  # Python
+    # ".rb",  # Ruby
+    # ".pl", ".pm",  # Perl
+    # ".r",   # R
+    # ".sh", ".bash",  # Shell
+    # ".ps1", ".psm1", ".psd1",  # PowerShell
+    # # "Makefile", "Makefile"  # Makefile
+    # # ".dockerfile", "Dockerfile"  # Dockerfile
+    # ".yaml", ".yml",  # YAML
+    # ".tcl",  # Tcl
+    # ".jl",  # Julia
+    # ".awk",  # Awk
+    # ".m",   # MATLAB
+    # ".coffee",  # CoffeeScript
+    # ".ex", ".exs",  # Elixir
+    # ".vim",  # Vim Script
+    # ".hs",  # Haskell
+}
+
 if __name__ == '__main__':
-    ext = fname[fname.find('.')+1:]
-    match(ext):
-        case 'py':
-            dc = Decommenter(fname, 'python')
-            if args.mode == 'decomment':
-                print(f'Decommenting {fname}...')
-                dc.decomment()
-            elif args.mode == 'recomment':
-                print(f'Recommenting {fname}...')
-                dc.recomment()
-        case _:
-            print(f'Filetype "{ext}" not supported.')
+    ext = fname[fname.find('.'):]
+    if ext in pythonstyle_langs:
+        dc = Decommenter(fname, 'python')
+        if args.mode == 'decomment':
+            print(f'Decommenting {fname}...')
+            dc.decomment()
+        elif args.mode == 'recomment':
+            print(f'Recommenting {fname}...')
+            dc.recomment()
+            
+    elif ext in cstyle_langs:
+        dc = Decommenter(fname, 'cstyle')
+        if args.mode == 'decomment':
+            print(f'Decommenting {fname}...')
+            dc.decomment()
+        elif args.mode == 'recomment':
+            print(f'Recommenting {fname}...')
+            dc.recomment()
+            
+    else:
+        print(f'Filetype "{ext}" not supported.')
--- decomment.py
+++ decomment.py
@@ -38,40 +38,131 @@ class Decommenter():
         with open(self.fname + '.dc.json', mode='w') as f:
             json.dump(comments, f)
     
-    # decomment code
+    # # decomment code
+    # def decomment(self):
+    #     # get original file
+    #     lines = self._get_code()
+        
+    #     dc_code, comments = [], []
+    #     for linenum, line in enumerate(lines, start=1):
+    #         # match comment that starts with a given symbol followed by exactly one space
+    #         # and the symbol is not preceded by a backslash
+    #         res = re.search(r'(?<!\\)' + self.symbol + r'\s(.*)', line)
+            
+    #         start_sym, end_sym = self.block_symbol
+    #         # TODO: handle block comments
+            
+    #         if res is not None:
+    #             # determine if it's an inline comment (if there's code before the comment)
+    #             inline = bool(line[:res.start()].strip())
+    #             comments.append({
+    #                 "inline":  inline,
+    #                 "linenum": linenum,
+    #                 "comment": res.group(1).strip()
+    #             })
+    #             if not inline:
+    #                 continue
+            
+    #         # remove comment from the code while retaining escaped cymbols
+    #         code_line = re.sub(r'(?<!\\)' + self.symbol + r'\s.*', '', line)
+    #         dc_code.append(code_line.rstrip() + '\n')  # Remove trailing whitespace only                 
+
+    #     self._write_code(dc_code)
+    #     self._write_comments(comments)
+            
+    #     print(f'{self.fname} decommented.')
+
     def decomment(self):
         # get original file
         lines = self._get_code()
         
         dc_code, comments = [], []
+        block_comment = False
+        block_comment_lines = []
+        
+        start_sym, end_sym = self.block_symbol
+        
         for linenum, line in enumerate(lines, start=1):
-            # match comment that starts with a given symbol followed by exactly one space
-            # and the symbol is not preceded by a backslash
-            res = re.search(r'(?<!\\)' + self.symbol + r'\s(.*)', line)
+            # Handle block comment start
+            res = re.search(r'(?<!\\)' + re.escape(start_sym) + r'(.*)', line)
             
-            # TODO: handle block comments
             
-            if res is not None:
-                # determine if it's an inline comment (if there's code before the comment)
-                inline = bool(line[:res.start()].strip())
-                comments.append({
-                    "inline":  inline,
-                    "linenum": linenum,
-                    "comment": res.group(1).strip()
-                })
-                if not inline:
+            # if not already in block comment and start symbol found
+            if not block_comment and res:
+                block_comment = True
+                s = res.group(1)
+                # this logic might leak certain situations involving a one-line block comment
+                # probably should convert to regex logic
+                # end_sym_idx = s.index(end_sym, res.pos) if end_sym in s else None
+                
+                end_sym_idx = re.search(r'(.*)(?<!\\)' + re.escape(end_sym), s).span()[0]
+                
+                # if end symbol found on same line (one-line block comment), extract 
+                # the block comment from the line and treat it as an inline comment
+                if end_sym_idx:
+                    block_comment = False
+                    comments.append({
+                        "inline":  True,
+                        "linenum": linenum,
+                        "comment": s[:end_sym_idx]
+                    })
+                    code_line = line[:res.span()[0]] + line[end_sym_idx + len(end_sym_idx):] # TODO: make regex?
+                    dc_code.append(code_line.rstrip() + '\n')  # Remove trailing whitespace only
                     continue
+                                
+                
+            # if in block comment and end symbol found (not on same line as start symbol)
+            if block_comment and res:
+                block_comment = False
+                comments.append({
+                        "inline":  True,
+                        "linenum": linenum,
+                        "comment": res.group(1)
+                    })
+                
+                # get whitespace of current line to indent the code and push code
+                ws = re.match(r"\s*", line).group(1)
+                code_line = re.sub(r'(.*)' + re.escape(end_sym), '', line)
+                dc_code.append(ws + code_line.rstrip() + '\n')  # Remove trailing whitespace only
+                    
             
-            # remove comment from the code while retaining escaped cymbols
-            code_line = re.sub(r'(?<!\\)' + self.symbol + r'\s.*', '', line)
-            dc_code.append(code_line.rstrip() + '\n')  # Remove trailing whitespace only                 
-
+            # if in block comment and no end symbol found
+            elif block_comment:
+                block_comment_lines.append({
+                    "inline":  False,
+                    "linenum": linenum,
+                    "comment": line.strip()
+                })
+                
+            # if not in block comment and no start symbol found
+            else:
+                # match inline comment that starts with a given symbol followed by exactly one space
+                # and the symbol is not preceded by a backslash
+                res = re.search(r'(?<!\\)' + re.escape(self.symbol) + r'\s(.*)', line)
+                
+                if res is not None:
+                    # determine if it's an inline comment (if there's code before the comment)
+                    inline = bool(line[:res.start()].strip())
+                    comments.append({
+                        "inline":  inline,
+                        "linenum": linenum,
+                        "comment": res.group(1).strip()
+                    })
+                    if not inline:
+                        continue
+                
+                # remove comment from the code while retaining escaped symbols
+                code_line = re.sub(r'(?<!\\)' + re.escape(self.symbol) + r'\s.*', '', line)
+                dc_code.append(code_line.rstrip() + '\n')  # Remove trailing whitespace only
+        
+        
         self._write_code(dc_code)
         self._write_comments(comments)
             
         print(f'{self.fname} decommented.')
-
-
+        
+        
+        
     # recomment python code
     def recomment(self):
         # load comments file
@@ -90,7 +181,7 @@ class Decommenter():
             if inline:
                 lines[linenum-1] = lines[linenum-1][:-1] + f'  {self.symbol} {comment}\n'
             else:
-                ws = re.match(r"\s*", lines[linenum-1]).group()
+                ws = re.match(r"\s*", lines[linenum-1]).group(1)
                 lines.insert(linenum - 1, f'{ws}# {comment}\n')
         
         # write changes over original code
