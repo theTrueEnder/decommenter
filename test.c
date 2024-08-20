f10d0a31c5907e0d3e218aeaafdd6570c85b7de3
theTrueEnder <67336179+theTrueEnder@users.noreply.github.com>
2024-08-20 13:48:47 -0400

    C test file
    Including test file while the decommenter is not completely functional for convenience (lost one version of it already). Will remove before MR.

---
+++ test.c
@@ -0,0 +1,31 @@
+int score = 0;
+int total_questions = 5;
+
+// NEWLINE Loop through questions and update score accordingly
+while (current_question < total_questions) {
+    /* BLOCK this is a normal block comment   */
+    string s = "//"; // INLINE /* and this
+    // NEWLINE is a
+    // NEWLINE multiline
+    // NEWLINE block comment
+    // NEWLINE commented out */
+    int score = 0;
+    s = '//'; // INLINE
+    s += '//';//INLINE
+    
+    // NEWLINE Evaluate user's answer and update score
+    if (answer_is_correct) { // INLINE other tricky situation *//*
+        score++;
+    }
+     
+    // NEWLINE Present the next question and repeat the process
+    printf("Score: %d/%d\n", score, total_questions);
+    // NEWLINE Display feedback and next question
+    printf("Next question:\n"); 
+}
+
+/*/* BLOCK this is
+a block comment /**/
+// NEWLINE with tricky things  */
+  // NEWLINE comment 2
+  // NEWLINE comment 1
